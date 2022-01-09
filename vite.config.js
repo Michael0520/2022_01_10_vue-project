9cf5c76db27b251a3cfa5caec18cd293e57d2f1f
MichaelLuo520 <po2319987@gmail.com>
2022-01-09 20:03:07 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { fileURLToPath, URL } from 'url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {
+  fileURLToPath,
+  URL
+} from 'url'
+import {
+  defineConfig
+} from 'vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    minify: true,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',
+        import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/<REPO>/' : './',
 })
